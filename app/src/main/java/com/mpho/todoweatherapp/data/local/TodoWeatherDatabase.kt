7af51126476922c9c61a3ca24d7dd2534ac84180
Mpho Ndlela <161179056+MphoCodes@users.noreply.github.com>
2025-10-17 09:47:52 +0200

    Updated database version to 2
    Updated TodoWeatherDatabase version from 1 to 2 to support future schema changes.

--- app/src/main/java/com/mpho/todoweatherapp/data/local/TodoWeatherDatabase.kt
+++ app/src/main/java/com/mpho/todoweatherapp/data/local/TodoWeatherDatabase.kt
@@ -11,7 +11,7 @@ import com.mpho.todoweatherapp.data.model.Converters
 
 @Database(
     entities = [Task::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
